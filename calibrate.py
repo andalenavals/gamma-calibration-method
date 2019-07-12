901e1016c333675be2f560c388bb3eea4874ad52
andalenavals <aanavarroa@unal.edu.co>
2019-07-12 00:58:17 -0300

    Essential function ready (AddFWH and Calibrate), Now is missing the xmin and xmax and the new chi2 function

--- calibrate.py
+++ calibrate.py
@@ -1,16 +1,22 @@
-import os 
+import os
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+plt.style.use('SVA1StyleSheet.mplstyle')
+
 def parse_args():
     import argparse
     parser = argparse.ArgumentParser(description='Alpha beta gamma test solving the fitting problem of system of equatiosn, plotting correlations and final correlation function with bias')
     
     parser.add_argument('--simfile',
-                        default='/data/publishing/gamma_calibration_method/gamma-calibration-method/data/sims/Sim_NaI2x2_22Na.dat',
+                        default='/data/publishing/gamma_calibration_method/gamma-calibration-method/data/sims/Sim_NaI3x3_22Na.dat',
                         help='.dat file of the Geant4 Simulation')
     parser.add_argument('--measurefile',
-                        default='/data/publishing/gamma_calibration_method/gamma-calibration-method/data/experiment/2015I(NaI2x2)/22Na_HV806_G200_GF0.5_ST_0.5_150s.dat',
+                        default='/data/publishing/gamma_calibration_method/gamma-calibration-method/data/experiment/Exp_NaI3x3_22Na.dat',
                         help='.dat file of the experimental data')
     parser.add_argument('--outpath', default='/data/publishing/gamma_calibration_method/gamma-calibration-method/plots',
                         help='location of the output of the files')
+    parser.add_argument('--plots', default=False, action='store_const', const=True, help='Do plots during the estimation of the parameters')
     args = parser.parse_args()
 
     return args
@@ -36,11 +42,15 @@ def AddFWHM(h, name, modelpars):
             hout.Fill(ran.Gaus(h.GetBinCenter(i), FWHM(h.GetBinCenter(i), modelpars)/2.35  ))
     return hout
 
+def CalFunction(x, calpars):
+    slope, intercept = calpars
+    return slope*x +  intercept
 def Rebin(h, name, newbinwidth, xlow):
+    from ROOT import TH1I,  TRandom
     ran = TRandom(123)
     binw = h.GetBinWidth(1)
     nbins = h.GetNbinsX()
-    xup = h.GetXaxis.GetBinUpEdge(nbins)
+    xup = h.GetXaxis().GetBinUpEdge(nbins)
     L = xup - xlow
     if( L%newbinwidth == 0 ):
       newnbin= int(L/newbinwidth)
@@ -51,25 +61,35 @@ def Rebin(h, name, newbinwidth, xlow):
       h2= TH1I(name , "" , newnbin , xlow , newxup )
     for i in range(1, nbins + 1):
         for j in range(1, int(h.GetBinContent(i)) + 1):
-	    h2.Fill( ran.Uniform(h.GetBinLowEdge(i) , h.GetXaxis().GetBinUpEdge(i) ) );
-  return h2
+            h2.Fill( ran.Uniform(h.GetBinLowEdge(i) , h.GetXaxis().GetBinUpEdge(i) ) );
+    return h2
       
-def Calibrate(h, name, ):
-TH1D *h2Q= (TH1D*) h->Clone(name);  
-  nbins= h2Q->GetNbinsX();
-  double new_bins[nbin+1];
-  for(int i=0; i <= nbin; i++)
-    new_bins[i] = pendiente*h2Q->GetBinLowEdge(i+1)+corte ;
-h2Q->SetBins(nbin, new_bins);
+def Calibrate(h, name, calpars, newbinwidth=None, xlow=None ):
+    import numpy as np
+    #We first create and histogram with no uniform binning 
+    h2= h.Clone(name)  
+    nbins= h2.GetNbinsX()
+    new_bins = np.array([ CalFunction( h2.GetBinLowEdge(i+1), calpars) for i in range (nbins + 1)])
+    h2.SetBins(nbins, new_bins)
+    #Then we rebin in a uniform bin histogram
+    if newbinwidth is not None:
+        name = '%s_rebin'%(name)  
+        h2 = Rebin(h2, name,  newbinwidth, xlow)
+    return h2
 
+def PrettyPlot(x, y, color='black', marker=None, label=None, xlabel='x-axis', ylabel='y-axis', alsize=24, legendsize=24, filepath='outplot.png'):
+    plt.plot(x, y, color=color, marker=marker,  label=label)
+    plt.xlim( [min(x),max(x)] )
+    plt.ylim(ymin=0)
+    plt.xlabel(xlabel, fontsize=alsize)
+    plt.ylabel(ylabel, fontsize=alsize)
+    plt.legend(loc='best', fontsize=legendsize)
+    plt.tight_layout()
+    print("Printing file: ", filepath)
+    plt.savefig(filepath)
+    
 def main():
     import numpy as np
-    import matplotlib
-    matplotlib.use('Agg')
-    import matplotlib.pyplot as plt
-    plt.style.use('SVA1StyleSheet.mplstyle')
-    #from ROOT import gROOT,  gSystem, TCanvas
-    
     args = parse_args()
 
     outpath = os.path.expanduser(args.outpath)
@@ -78,67 +98,52 @@ def main():
             os.makedirs(outpath)
     except OSError:
         if not os.path.exists(outpath): raise
-    
+
+    #Save data in arrays
     ch_sim,  counts_sim = np.loadtxt(args.simfile, dtype=int,  unpack=True)
     ch_exp,  counts_exp = np.loadtxt(args.measurefile, dtype=int,  unpack=True)
 
-    #Printing Original data.
-    #Simulation
-    '''
-    plt.clf()
-    plt.plot(ch_sim, counts_sim, color='blue', marker=None,  label='Simulation')
-    plt.xlim( [min(ch_sim),max(ch_sim)] )
-    plt.xlabel(r'Energy (keV)', fontsize=24)
-    plt.ylabel(r'Counts', fontsize=24)
-    plt.legend(loc='best', fontsize=24)
-    plt.tight_layout()
-    filepath = os.path.join(outpath, 'original_sim.png')
-    print("Printing file: ", filepath)
-    plt.savefig(filepath)
-
-    #Experiment
-    plt.clf()
-    plt.plot(ch_exp, counts_exp, color='red', marker=None,  label='Experiment')
-    plt.xlim( [min(ch_exp),max(ch_exp)] )
-    plt.xlabel(r'Channel', fontsize=24)
-    plt.ylabel(r'Counts', fontsize=24)
-    plt.legend(loc='best', fontsize=24)
-    plt.tight_layout()
-    filepath = os.path.join(outpath, 'original_exp.png')
-    print("Printing file: ", filepath)
-    plt.savefig(filepath)
-    '''
     nbins = len(ch_exp)
     xlow =  min(ch_sim); xup = max(ch_sim)
     hsim = SaveInTH1(ch_sim, counts_sim, 'hist_sim', nbins, xlow, xup)
-    pars = [0.027, 1.2,  0] #a,b,c
-    hsim = AddFWHM(hsim, 'hist_sim_fwhm',  pars)
+    pars = [0.027, 1.2,  0] #aE + b*sqrt(E)+c
+    hsim_fwhm = AddFWHM(hsim, 'hist_sim_fwhm',  pars)
+    ch_sim_fwhm = [ hsim_fwhm.GetBinCenter(i) for i in range(1, hsim_fwhm.GetNbinsX() + 1)]
+    counts_sim_fwhm =  [hsim_fwhm.GetBinContent(i) for i in range(1, hsim_fwhm.GetNbinsX() + 1)]
+    calpars =  [1.3 , -15.6  ] #mx+d
+    hsim_fwhm_ch = Calibrate(hsim_fwhm, 'hist_sim_fwhm_cal', calpars, newbinwidth=1, xlow=0)
+    ch_sim_fwhm_ch = [ hsim_fwhm_ch.GetBinCenter(i) for i in range(1, hsim_fwhm_ch.GetNbinsX() + 1)]
+    counts_sim_fwhm_ch =  [hsim_fwhm_ch.GetBinContent(i) for i in range(1, hsim_fwhm_ch.GetNbinsX() + 1)]
+    print(hsim_fwhm_ch.GetNbinsX())
+
     
-    ch_sim = [ hsim.GetBinCenter(i) for i in range(1, nbins + 1)]
-    counts_sim =  [hsim.GetBinContent(i) for i in range(1, nbins + 1)]
-    plt.clf()
-    plt.plot(ch_sim, counts_sim, color='blue', marker=None,  label='Simulation with GEB')
-    plt.xlim( [min(ch_sim),max(ch_sim)] )
-    plt.xlabel(r'Energy (keV)', fontsize=24)
-    plt.ylabel(r'Counts', fontsize=24)
-    plt.legend(loc='best', fontsize=24)
-    plt.tight_layout()
-    filepath = os.path.join(outpath, 'simTH1fwhm.png')
-    print("Printing file: ", filepath)
-    plt.savefig(filepath)
+
+    if(args.plots):
+        #Original Simulation
+        plt.clf()
+        filepath = os.path.join(outpath, 'original_sim.png')
+        PrettyPlot(ch_sim, counts_sim, color='blue', marker=None, label='Simulation', xlabel='Energy (keV)', ylabel='Counts', alsize=24, legendsize=24, filepath=filepath)
+        #Original Experiment
+        plt.clf()
+        filepath = os.path.join(outpath, 'original_exp.png')
+        PrettyPlot(ch_exp, counts_exp, color='red', marker=None, label='Experiment', xlabel='Channel', ylabel='Counts', alsize=24, legendsize=24, filepath=filepath)
+        #Sim with FWHM
+        plt.clf()
+        filepath = os.path.join(outpath, 'sim_fwhm.png')
+        PrettyPlot(ch_sim_fwhm, counts_sim_fwhm, color='blue', marker=None, label='Simulation with GEB', xlabel='Energy (keV)', ylabel='Counts', alsize=24, legendsize=18, filepath=filepath)
+        #Sim with FWHM in channels
+        plt.clf()
+        filepath = os.path.join(outpath, 'sim_fwhm_ch.png')
+        PrettyPlot(ch_sim_fwhm_ch, counts_sim_fwhm_ch, color='blue', marker=None, label='Simulation with GEB', xlabel='Channel', ylabel='Counts', alsize=24, legendsize=18, filepath=filepath)
+
     
     
-    '''
-    c1 =  TCanvas('c1', '', 800, 600)
-    c1.SetBottomMargin( 0.15 )
-    c1.SetTopMargin( 0.05 )
-    c1.SetLeftMargin( 0.15 )
-    c1.SetRightMargin( 0.15 )
     
-    hsim.Draw('')
-    filepath = os.path.join(outpath, 'simFWHM.png')
-    c1.Print(filepath)
-    '''
+    
+    
+  
+    
+ 
         
         
    
